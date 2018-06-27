0292d4e892714b98ac4ef447f5e884ad1e3867a3
MsldLuxy <310870966@qq.com>
2018-06-27 12:04:13 +0800

    mobile mode

--- public/js/music.js
+++ public/js/music.js
@@ -587,16 +587,15 @@ function getLrc(data){
 }
 //  播放时，旋转 播放按钮
 function load() {  // 当浏览器能够开始播放指定的音频/视频时, 该视频已准备好开始播放，发生canplay事件
-    myMusic.load();
-    myMusic.addEventListener('canplay',function () {
+    // myMusic.addEventListener('canplay',function () {
         setTimeout(function () {
             myMusic.play();
             mark = false;  //暂停状态为false即正在播放音乐
             cd.className = 'cd rotate';
             //loading.style.display = 'none';
-        },100);
+        },200);
         btn.style.backgroundImage = 'url(public/images/play.png)';
-    })
+    // })
 }
 
 
