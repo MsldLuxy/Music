ad74fc7273c784b9d1cc848ee9d7c9a671adb3ea
MsldLuxy <310870966@qq.com>
2018-06-27 12:00:43 +0800

    mobile mode

--- public/js/music.js
+++ public/js/music.js
@@ -587,7 +587,8 @@ function getLrc(data){
 }
 //  播放时，旋转 播放按钮
 function load() {  // 当浏览器能够开始播放指定的音频/视频时, 该视频已准备好开始播放，发生canplay事件
-    // myMusic.addEventListener('canplay',function () {
+    myMusic.load();
+    myMusic.addEventListener('canplay',function () {
         setTimeout(function () {
             myMusic.play();
             mark = false;  //暂停状态为false即正在播放音乐
@@ -595,7 +596,7 @@ function load() {  // 当浏览器能够开始播放指定的音频/视频时, �
             //loading.style.display = 'none';
         },100);
         btn.style.backgroundImage = 'url(public/images/play.png)';
-    // })
+    })
 }
 
 
